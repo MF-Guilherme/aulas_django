2042662702004665e92f88d306f8b64bd5b76abc
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-01-31 22:27:05 -0300

    Pegando dados do client pela URL e utilizando no backend pelo parâmetro GET

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def cadastro(request):
-    return render(request, 'cadastro/index.html')
+    # exemplo de URL: http://127.0.0.1:8000/autenticacao/cadastro/?nome=Guilherme&sobrenome=Montenegro&idade=32
+    
+    # pegando dados da url pelo parâmetro request
+    nome = request.GET.get('nome')
+    sobrenome = request.GET.get('sobrenome')
+    idade = request.GET.get('idade')
+    return render(request, 'cadastro/index.html', {'nome': nome, 'sobrenome': sobrenome, 'idade': idade})
