d30ca8c495366ec14fb6386c7e36ab412d884202
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-02-02 21:42:17 -0300

    Unica URL que faz uma coisa quando o method é GET e outra coisa quando é POST

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -3,16 +3,9 @@ from django.http import HttpResponse
 import json
 
 def cadastro(request):
-    # exemplo de URL: http://127.0.0.1:8000/autenticacao/cadastro/?nome=Guilherme&sobrenome=Montenegro&idade=32
-    
-    # pegando dados da url pelo parâmetro request
-    nome = request.GET.get('nome')
-    sobrenome = request.GET.get('sobrenome')
-    idade = request.GET.get('idade')
-    return render(request, 'cadastro/index.html', {'nome': nome, 'sobrenome': sobrenome, 'idade': idade})
-
-
-def valida_formulario(request):
-    nome = request.POST.get('nome')
-    email = request.POST.get('email')
-    return HttpResponse(json.dumps({'nome': nome, 'email': email})) 
+    if request.method == "GET":
+        return render(request, 'cadastro/index.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        return HttpResponse(json.dumps({'nome': nome, 'email': email}))
